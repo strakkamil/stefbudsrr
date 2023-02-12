9134926213aaec8d41cf7fac526a5618d895ba0e
strakkamil <strakkamil@protonmail.com>
2023-02-12 23:14:32 +0100

    Demo

--- src/app/config.ts
+++ src/app/config.ts
@@ -3,6 +3,8 @@ export const config = {
 		{
 			"category": 'Łazienka',
 			"isPromoted": true,
+			"description": 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo fugiat dolor magnam vel accusantium delectus corporis necessitatibus cum laboriosam non voluptatibus distinctio id ad vitae maxime, velit aliquam ipsum facere! Quod eveniet repellat doloremque eos nesciunt aut recusandae dignissimos? Numquam omnis alias quidem illum! Accusantium temporibus hic nam, laboriosam quibusdam eligendi harum magnam, autem, expedita dolorem inventore totam architecto aliquid!',
+			"materials": ['drewno', 'stal', 'żelazo'],
 			"photos": [
 				{ "src": 'hero-2.jpg' },
 				{ "src": 'kitchen-1.jpg' },
@@ -23,6 +25,8 @@ export const config = {
 		{
 			"category": 'Kuchnia',
 			"isPromoted": true,
+			"description": 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo fugiat dolor magnam vel accusantium delectus corporis necessitatibus cum laboriosam non',
+			"materials": ['drewno buk', 'miedź', 'żelazo'],
 			"photos": [
 				{ "src": 'hero-1.jpg' },
 				{ "src": 'hero-2.jpg' },
@@ -38,6 +42,8 @@ export const config = {
 		{
 			"category": 'Sypialnia',
 			"isPromoted": false,
+			"description": 'Explicabo fugiat dolor magnam vel accusantium delectus corporis necessitatibus cum laboriosam non voluptatibus distinctio id ad vitae maxime, velit aliquam ipsum facere! Quod eveniet repellat doloremque eos nesciunt aut recusandae dignissimos? Numquam omnis alias quidem illum! Accusantium temporibus hic nam, laboriosam quibusdam eligendi harum magnam, autem, expedita dolorem inventore totam architecto aliquid!',
+			"materials": ['papier', 'stal', 'żelazo'],
 			"photos": [
 				{ "src": 'main-1.jpg' },
 				{ "src": 'hero-2.jpg' },
@@ -57,6 +63,8 @@ export const config = {
 		{
 			"category": 'Łazienka',
 			"isPromoted": false,
+			"description": 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo fugiat dolor magnam vel accusantium delectus corporis necessitatibus cum laboriosam non voluptatibus distinctio id ad vitae maxime, velit aliquam ipsum facere! Quod eveniet repellat doloremque eos nesciunt aut recusandae dignissimos? Numquam omnis alias quidem illum! Accusantium temporibus hic nam, laboriosam quibusdam eligendi harum magnam, autem, expedita dolorem inventore totam architecto aliquid!',
+			"materials": ['drewno', 'stal', 'ruda'],
 			"photos": [
 				{ "src": 'hero-1.jpg' },
 				{ "src": 'hero-2.jpg' },
@@ -72,6 +80,25 @@ export const config = {
 		{
 			"category": 'Kuchnia',
 			"isPromoted": true,
+			"description": 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo fugiat dolor magnam vel accusantium delectus corporis necessitatibus cum laboriosam non voluptatibus distinctio id ad vitae maxime, velit aliquam ipsum facere! Quod eveniet repellat doloremque eos nesciunt aut recusandae dignissimos? Numquam omnis alias quidem illum! Accusantium temporibus hic nam, laboriosam quibusdam eligendi harum magnam, autem, expedita dolorem inventore totam architecto aliquid!',
+			"materials": ['drewno', 'stal', 'żelazo'],
+			"photos": [
+				{ "src": 'hero-1.jpg' },
+				{ "src": 'hero-2.jpg' },
+				{ "src": 'kitchen-1.jpg' },
+				{ "src": 'main-1.jpg' },
+				{ "src": 'main-1.jpg' },
+				{ "src": 'main-2.jpg' },
+				{ "src": 'main-3.jpg' },
+			],
+			"videos": [
+			]
+		},
+		{
+			"category": 'Pozostałe',
+			"isPromoted": true,
+			"description": 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo fugiat dolor magnam vel accusantium delectus corporis necessitatibus cum laboriosam non voluptatibus distinctio id ad vitae maxime, velit aliquam ipsum facere! Quod eveniet repellat doloremque eos nesciunt aut recusandae dignissimos? Numquam omnis alias quidem illum! Accusantium temporibus hic nam, laboriosam quibusdam eligendi harum magnam, autem, expedita dolorem inventore totam architecto aliquid!',
+			"materials": ['drewno', 'stal', 'żelazo'],
 			"photos": [
 				{ "src": 'hero-1.jpg' },
 				{ "src": 'hero-2.jpg' },
